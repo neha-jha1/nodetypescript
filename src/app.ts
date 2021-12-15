8a0129cb549508ab8f95fdecc1ffd68eb4ca06f6
nehajha1996 <64786332+nehajha1996@users.noreply.github.com>
2021-12-15 13:19:18 +0530

    intial commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('test')
+import express from 'express';
+import dotenv from 'dotenv';
+const app  = express();
+
+app.get('/',(req,res) => {
+    res.send("home");
+})
+
+app.listen(3000, ()=> {
+    console.log('app working')
+})
